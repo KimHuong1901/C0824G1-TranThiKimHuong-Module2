1227625916e78bdf57db801b876604d74e94438e
KimHuong1901 <trankimhuong1901@gmail.com>
2024-10-15 09:41:25 +0700

    ss5

--- Module2/src/ss5/Circle.java
+++ Module2/src/ss5/Circle.java
@@ -1,20 +1,26 @@
 package ss5;
 
-public class TestCircle {
+public class Circle {
     private double radius = 1.0;
     private String color = "red";
-    public TestCircle(){}
-    public TestCircle(double radius) {
+    public Circle(){}
+    public Circle(double radius) {
         this.radius = radius;
     }
-    public double getRadius() {
+    protected double getRadius() {
         return radius;
     }
-    public double getArea() {
+    protected double getArea() {
         return Math.PI * radius * radius;
     }
-
+}
+class TestCircle{
     public static void main(String[] args) {
-
+       Circle test = new Circle();
+        test.getArea();
+        test.getRadius();
+        System.out.println("Area is " + test.getArea() + " and radius is " + test.getRadius());
     }
 }
+
+
--- Module2/src/ss5/Student.java
+++ Module2/src/ss5/Student.java
@@ -1,9 +1,36 @@
 package ss5;
 
+import java.util.Scanner;
+
 public class Student {
-//    private String name;
-//    private String classes;
-//public void String setName(String name){
-//    this.name = name;
-//}
+    private String name = "John";
+    private String classes = "CO2";
+    public Student() {}
+    protected void setName(String name){
+        this.name = name;
+    }
+    protected void setClasses(String classes){
+        this.classes = classes;
+    }
+    // Test giá trị trả về
+//    public String getName(){
+//        return this.name;
+//    }
+//    public String getClasses(){
+//        return this.classes;
+//    }
+}
+ class Test {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the name: ");
+       String name = sc.nextLine();
+       System.out.println("Enter the class: ");
+       String classes = sc.nextLine();
+       Student s2 = new Student();
+       s2.setName(name);
+       s2.setClasses(classes);
+        //In ra kết quả với 2 phương thức set có access modifier là public:
+//       System.out.println("Name is: " + s2.getName() + "and " + "class is: " + s2.getClasses());
+     }
 }
