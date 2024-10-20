e4deee94b322f3d769dfb3fd69ac06271ff6d867
KimHuong1901 <trankimhuong1901@gmail.com>
2024-10-20 12:53:49 +0700

    TennisGame

--- Module2/src/ss8/bai_tap/Ex.java
+++ Module2/src/ss8/bai_tap/Ex.java
@@ -1,33 +1,31 @@
 package ss8.bai_tap;
 
-public class TennisGame {
-
+public class Ex {
     public static String getScore( int scoreFirstPlayer, int scoreSencondPlayer) {
         String score = "";
         int tempScore;
         if (scoreFirstPlayer==scoreSencondPlayer)
         {
-            switch (scoreFirstPlayer)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-
-            }
+//            switch (scoreFirstPlayer)
+//            {
+//                case 0:
+//                    score = "Love-All";
+//                    break;
+//                case 1:
+//                    score = "Fifteen-All";
+//                    break;
+//                case 2:
+//                    score = "Thirty-All";
+//                    break;
+//                case 3:
+//                    score = "Forty-All";
+//                    break;
+//                default:
+//                    score = "Deuce";
+//                    break;
+//
+//            }
         }
-
         else if (scoreFirstPlayer>=4 || scoreSencondPlayer>=4)
         {
             int minusResult = scoreFirstPlayer-scoreSencondPlayer;
@@ -53,21 +51,7 @@ public class TennisGame {
                 } else {
                     score+="-"; tempScore = scoreSencondPlayer;
                 }
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
+
             }
         }
         return score;
---
+++ Module2/src/ss8/bai_tap/Score.java
@@ -0,0 +1,24 @@
+package ss8.bai_tap;
+
+public class Score {
+    public static String setScore(int score) {
+        String result = "";
+        switch(score)
+        {
+            case 0:
+                result+="Love";
+                break;
+            case 1:
+                result+="Fifteen";
+                break;
+            case 2:
+                result+="Thirty";
+                break;
+            case 3:
+                result+="Forty";
+                break;
+        }
+        return result;
+    }
+    }
+
